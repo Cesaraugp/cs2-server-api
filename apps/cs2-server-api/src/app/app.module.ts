88472a37ddf82e4ba89b9ea9ddc13712615ea81d
Cesar Pérez <cesaraugp01@gmail.com>
2025-02-09 14:06:19 -0500

    add RconModule to AppMdoule

--- apps/cs2-server-api/src/app/app.module.ts
+++ apps/cs2-server-api/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { RconMod } from './rcon/rcon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+      load: [configuration],
+      isGlobal: true,
+    }),
+    RconMod,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
